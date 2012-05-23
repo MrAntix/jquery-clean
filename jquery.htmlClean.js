f2374b4c1be56448c00f0325b3bc696ea97201bf
antixsoftware <antixsoftware@a2e515d2-8e27-11dd-aa30-2b96cd1846c6>
2012-05-23 09:53:47 +0000

    fix for issue 15

--- jquery.htmlClean.js
+++ jquery.htmlClean.js
@@ -33,7 +33,7 @@ Use and distibution http://www.opensource.org/licenses/bsd-license.php
         options = $.extend({}, $.htmlClean.defaults, options);
 
         var tagsRE = /<(\/)?(\w+:)?([\w]+)([^>]*)>/gi;
-        var attrsRE = /(\w+)=(".*?"|'.*?'|[^\s>]*)/gi;
+        var attrsRE = /([\w\-]+)=(".*?"|'.*?'|[^\s>]*)/gi;
 
         var tagMatch;
         var root = new Element();
