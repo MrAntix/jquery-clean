650aea622ffcee1d27b213d94d79acbb78175924
antixsoftware <antixsoftware@a2e515d2-8e27-11dd-aa30-2b96cd1846c6>
2013-07-11 09:43:07 +0000

    fix for issue 30 allow spaces around attribute equals signs

--- trunk/jquery.htmlClean.js
+++ trunk/jquery.htmlClean.js
@@ -35,7 +35,7 @@ Use and distibution http://www.opensource.org/licenses/bsd-license.php
         options = $.extend({}, $.htmlClean.defaults, options);
 
         var tagsRE = /(<(\/)?(\w+:)?([\w]+)([^>]*)>)|<!--(.*?--)>/gi;
-        var attrsRE = /([\w\-]+)=(".*?"|'.*?'|[^\s>]*)/gi;
+        var attrsRE = /([\w\-]+)\s*=\s*(".*?"|'.*?'|[^\s>\/]*)/gi;
 
         var tagMatch;
         var root = new Element();
